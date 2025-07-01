4212036a7fc017d23daa5ffba09f4e022dead72b
OldMartijntje_ubuntu <oldmartijntje@gmail.com>
2025-07-01 17:03:01 +0200

    design the discovery of mqtt lobbies

--- src/services/MemoryService.ts
+++ src/services/MemoryService.ts
@@ -1,16 +1,21 @@
 import {SystemLogic} from "../systemModels/SystemLogic.ts";
 import {getRandomFiveCharNumber} from "../helpers/RandomisationHelper.ts";
 
-export interface LobbySettings {
+export interface LobbySettingsInterface {
     isActive: boolean; // whether this model is actually used.
     lobbyIdentifier: string; // the id you can enter in the join menu.
     discoverable: boolean; // whether you listen to `discoverableLobbyIdentifier` or not
     discoverableLobbyIdentifier: string; // the id that is discoverable
 }
 
+export interface LobbyInfoInterface {
+
+}
+
 class MemoryService extends SystemLogic {
     private _username: string
-    private _lobby: LobbySettings
+    private _lobby: LobbySettingsInterface
+    private _connectionData: ConnectionData
 
     constructor() {
         super()
@@ -22,15 +27,25 @@ class MemoryService extends SystemLogic {
             discoverable: false,
             discoverableLobbyIdentifier: ''
         };
+        this._connectionData = new ConnectionData();
     }
 
-    public setLobby(lobby: LobbySettings): void {
+    public setLobby(lobby: LobbySettingsInterface): void {
         this._logger.StringifyObject(lobby).PrependText(`Set lobby value to: "`).AppendText('"').LogDebug();
         this._lobby = lobby;
     }
 
-    public getLobby(): LobbySettings {
+    public getLobby(): LobbySettingsInterface {
         return this._lobby;
+    }
+
+    public setConnData(connData: ConnectionData): void {
+        this._logger.StringifyObject(connData).PrependText(`Set connData value to: "`).AppendText('"').LogDebug();
+        this._connectionData = connData;
+    }
+
+    public getConnData(): ConnectionData {
+        return this._connectionData;
     }
 
     // Getter for username
@@ -43,6 +58,22 @@ class MemoryService extends SystemLogic {
         this._logger.LogDebug(`Set username value to: "${value}"`)
         localStorage.setItem("tiny-soup:username", value);
         this._username = value;
+    }
+}
+
+class ConnectionData extends SystemLogic {
+    private _discovery: LobbyInfoInterface[] = [];
+    constructor() {
+        super()
+    }
+
+    public setDiscoveryLobbyInfo(lobbyInfo: LobbyInfoInterface[]): void {
+        this._logger.StringifyObject(lobbyInfo).PrependText(`Set lobbyInfo value to: "`).AppendText('"').LogDebug();
+        this._discovery = lobbyInfo;
+    }
+
+    public getDiscoveryLobbyInfo(): LobbyInfoInterface[] {
+        return this._discovery;
     }
 }
 
